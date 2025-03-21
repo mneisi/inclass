4517dc958a11dc5e68bac2581bc27953bbd2461d
Mostafa Neisi <131492781+mneisi@users.noreply.github.com>
2025-03-21 18:21:11 +0200

    Add files via upload

--- inlcass/tasks/urls.py
+++ inlcass/tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
 from . import views 
 
+app_name = 'tasks'
+
 urlpatterns = [
     path('tasks/', views.index, name='index'),
     path('tasks/add', views.add, name='add'),
 ]
+
