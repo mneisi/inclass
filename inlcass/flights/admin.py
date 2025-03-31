133beeb5b41eb72d00f446b383f163e70cbfea14
Mostafa Neisi <131492781+mneisi@users.noreply.github.com>
2025-03-31 18:17:00 +0300

    Add files via upload

---
+++ inlcass/flights/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Flight, Airport
+
+admin.site.register(Airport)
+admin.site.register(Flight)
--- inlcass/inclass/settings.py
+++ inlcass/inclass/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home',
     'tasks',
+    'flights',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
